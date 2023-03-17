20d850238ced19bba29622fad021f8e39b6a8ef4
MacBook <macbook@MacBook-Air-MacBook.local>
2023-03-18 03:39:00 +0600

    2

---
+++ main.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'{message.from_user.first_name},Hello')
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply("ti tupoi?")
+
+if __name__ == '__main__':
+        executor.start_polling(dp)
