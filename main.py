661d4ac1e728fb1ec556aaf6d1c20e07ebb077df
macbook <azimkanovbaktiar@gmail.com>
2023-03-18 16:06:18 +0600

    почти 2

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
 main = ReplyKeyboardMarkup(resize_keyboard=True)
-main.add('Гдз').add('Нейросети').add('Помощь создателю')
+main.add('Гдз').add('Нейросети').add('Чат').add('Помощь создателю')
 
 main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
-main_admin.add('Гдз').add('Нейросети').add('Помощь создателю').add('Админ панель')
+main_admin.add('Гдз').add('Нейросети').add('Чат').add('Помощь создателю').add('Админ панель')
 
 adminpanel = ReplyKeyboardMarkup(resize_keyboard=True)
 adminpanel.add('Добавить гдз').add('Удалить гдз').add('Сделать рассылку').add('Назад')
 
 
 seven = ReplyKeyboardMarkup(resize_keyboard=True)
-seven.add('Алгебра').add('Англиский').add('').add('Геометрия').add('Назад')
+seven.add('Алгебра').add('Геометрия').add('Остальные предметы').add('Назад')
 
 ai = ReplyKeyboardMarkup(resize_keyboard=True)
 ai.add('chatGPT').add('DALL-E').add('ReText').add('Назад')
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer(f'{message.from_user.first_name},Добро пожаловать на бота ')
+    await message.answer(f'{message.from_user.first_name}, Добро пожаловать! Этот бот создан для помощи учеников и просто для фана ')
     await message.answer_sticker('CAACAgIAAxkBAAMaZBTgJnIRON_pM7EpHkJpmyubsB8AAgUAAwsieTNT4xP8FX5BVS8E',
                                  reply_markup=main)
     if message.from_user.id == int(os.getenv('ADMIN_ID')):
@@ -49,15 +49,14 @@ async def gdz(message: types.Message):
 async def alg(message: types.Message):
     await message.answer(f"Вот гдз по алгебре 7 класса: http://surl.li/foimj")
 
-@dp.message_handler(text='Англиский')
-async def eng(message: types.Message):
-    await message.answer(f"Пока что не доступно. Если у вас есть гдз то напишите создателю")
 
 @dp.message_handler(text='Геометрия')
 async def geom(message: types.Message):
     await message.answer(f"Вот гдз по алгебре 7 класса: http://surl.li/foiou")
 
-
+@dp.message_handler(text='Остальные предметы')
+async def other(message: types.Message):
+    await message.answer(f'Для других предеметов используйте нейросети', reply_markup=ai)
 
 @dp.message_handler(text='Нейросети')
 async def bot(message: types.Message):
@@ -79,6 +78,10 @@ async def retext(message: types.Message):
 @dp.message_handler(text='Помощь создателю')
 async def contacts(message: types.Message):
     await message.answer(f"Вы можете отправить ему сайты с гдз для каких то предметов или просто сообщить об ошибке или идею: @soquoru")
+
+@dp.message_handler(text='Чат')
+async def chat(message: types.Message):
+    await message.answer(f"Чат с учениками 69 гимазии: https://t.me/+MqaYd4TYTfI2ZGM6")
 
 @dp.message_handler(text='Назад')
 async def back(message: types.Message):
